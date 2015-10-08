82b584d898574a9e77fb9c27e2e4618fb7dedf85
rooney1237 <rooney123777@abv.bg>
2015-10-08 22:57:47 +0300

    Added Tables
    Added Tables

---
+++ LiquiBLQ.Server.WebAPI/src/test/java/com/shadows/liquiblq/webapi/tests/SongControllerTests.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shadows.liquiblq.webapi.tests;
+
+import java.io.InputStream;
+import javax.print.DocFlavor;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author toshiba
+ */
+public class SongControllerTests {
+    
+    public SongControllerTests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void FileRead() throws Exception{
+        int i;
+        InputStream instr = new InputStream();
+        while ((i = instr.read()) != -1) {
+            osstr.write(i);
+        }
+
+        instr.close();
+        osstr.close();
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
